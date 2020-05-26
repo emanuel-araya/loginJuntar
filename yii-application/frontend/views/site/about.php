49d1dc6c435992ea34ba502df736ae9ed461681b
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-05-26 03:15:18 -0300

    algo de texto

--- yii-application/frontend/views/site/about.php
+++ yii-application/frontend/views/site/about.php
@@ -9,5 +9,5 @@ $this->title = 'Acerca';
 <div class="site-about">
     <h1><?= Html::encode($this->title). " de Proyecto Juntar" ?></h1>
 
-    <p>El Proyecto Juntar comenzo como una aplicacion que le permitiria gestionar sus eventos y ahi quedo</p>
+    <p>El Proyecto Juntar comenzo como una aplicacion que le permitiria gestionar sus eventos, luego agrego nuveas funcionalidades como diferentes roles de usaurios etc...</p>
 </div>
